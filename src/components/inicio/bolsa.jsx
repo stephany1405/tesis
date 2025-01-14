d81d4a039eb84e4195c42d82a5ad640a7aa78ef0
Jhonangel Briceño <jhonangelbriceo04@gmail.com>
2025-01-14 13:43:25 -0400

    bolsa.jsx

--- src/components/inicio/bolsa.jsx
+++ src/components/inicio/bolsa.jsx
@@ -26,10 +26,6 @@ const useCheckout = (total, cartItems, selectedItems, resetCart) => {
   const [isProcessing, setIsProcessing] = useState(false);
   const [redirect, setRedirect] = useState(false);
   const navigate = useNavigate();
-  const [showHomeService, setShowHomeService] = useState(false);
-  const [address, setAddress] = useState('');
-  const [showCalendar, setShowCalendar] = useState(false);
-
   useEffect(() => {
     if (redirect) {
       navigate("/checkout/success", { replace: true });
@@ -75,7 +71,6 @@ const useCheckout = (total, cartItems, selectedItems, resetCart) => {
           amount: Math.round(total * 100),
           products: cartItems.filter((_, index) => selectedItems[index]),
           noteOfServices: note,
-          paymentMethod: "cash",
         }
       );
 
@@ -166,7 +161,7 @@ const Bolsa = () => {
   const [paymentMethod, setPaymentMethod] = useState("Tarjeta");
   const [loadingCash, setLoadingCash] = useState(false);
   const [redirect, setRedirect] = useState(false);
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (redirect) {
@@ -176,12 +171,12 @@ const Bolsa = () => {
   useEffect(() => {
     if (loadingCash) {
       setTimeout(() => {
-        setLoadingCash(false); 
-        setRedirect(true);  
+        setLoadingCash(false);
+        setRedirect(true);
       }, 2000);
     }
   }, [loadingCash]);
-  
+
   useEffect(() => {
     setSelectedItems(cartItems.map(() => true));
   }, [cartItems]);
@@ -224,107 +219,8 @@ const Bolsa = () => {
     });
   };
 
-<<<<<<< HEAD
   const handleCashPayment = () => {
     setLoadingCash(true);
-=======
-  const handleHomeServiceToggle = () => {
-    setShowHomeService(!showHomeService);
-  };
-
-  const handleCalendarToggle = () => {
-    setShowCalendar(!showCalendar);
-  };
-
-  const [showForm, setShowForm] = useState(false);
-
-  const toggleForm = () => {
-    setShowForm(!showForm);
-  };
-
-  const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLIC_KEY);
-
-  const useCheckout = (total) => {
-    const stripe = useStripe();
-    const elements = useElements();
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
-
-    const processPayment = async () => {
-      if (!stripe || !elements) {
-        throw new Error("Stripe.js aún no se ha cargado.");
-      }
-
-      const cardElement = elements.getElement(CardElement);
-      if (!cardElement) {
-        throw new Error("CardElement no encontrado.");
-      }
-
-      const { error: paymentError, paymentMethod } =
-        await stripe.createPaymentMethod({
-          type: "card",
-          card: cardElement,
-        });
-
-      if (paymentError) {
-        throw paymentError;
-      }
-
-      const { id } = paymentMethod;
-
-      const response = await axios.post(
-        "http://localhost:3000/api/orden/checkout",
-        {
-          id: id,
-          amount: Math.round(total * 100),
-          products: cartItems.filter((_, index) => selectedItems[index]),
-        }
-      );
-
-      return response.data;
-    };
-
-    const handleSubmit = async (e) => {
-      e.preventDefault();
-      setLoading(true);
-      setError(null);
-
-      try {
-        const data = await processPayment();
-        console.log("Pago exitoso:", data);
-        elements.getElement(CardElement).clear();
-        resetCart();
-        navigate("/success");
-      } catch (err) {
-        console.error("Error durante la verificación:", err);
-        setError(err.message || "Algo salió mal.");
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    return { handleSubmit, loading, error };
-  };
-
-  const CheckOutForm = ({ total }) => {
-    const { handleSubmit, loading, error } = useCheckout(total);
-
-    return (
-      <form onSubmit={handleSubmit} className={styles.paymentForm}>
-        <div className={styles.cardElementContainer}>
-          <CardElement options={{ hidePostalCode: true }} />
-        </div>
-        {error && <div className={styles.error}>{error}</div>}
-        <button
-          type="submit"
-          className={styles.paymentButton}
-          disabled={loading}
-        >
-          {loading ? <span className={styles.loader}></span> : "Pagar"}
-        </button>
-      </form>
-    );
->>>>>>> 300293674121452163b021140472e18fea699c95
   };
 
   return (
@@ -353,7 +249,6 @@ const Bolsa = () => {
             </div>
           </div>
 
-<<<<<<< HEAD
           {cartItems.map((item, index) => (
             <div key={index} className={styles.item}>
               <input
@@ -394,55 +289,6 @@ const Bolsa = () => {
                     Eliminar
                   </button>
                 </div>
-=======
-          <div className={styles.orderSummary}>
-            <div className={styles.homeServiceSection}>
-              <button onClick={handleHomeServiceToggle} className={styles.homeServiceButton}>
-                {showHomeService ? 'Desactivar servicio a domicilio' : 'Activar servicio a domicilio'}
-              </button>
-              {showHomeService && (
-                <div className={styles.addressInputContainer}>
-                  <input
-                    type="text"
-                    value={address}
-                    onChange={(e) => setAddress(e.target.value)}
-                    placeholder="Introduce tu dirección"
-                    className={styles.addressInput}
-                  />
-                  <div className={styles.mapPlaceholder}>
-                    {/* Aquí irá el mapa */}
-                    <p>Mapa se mostrará aquí</p>
-                  </div>
-                </div>
-              )}
-            </div>
-            
-            <div className={styles.scheduleServiceSection}>
-              
-                <p> Agenda tu servicio</p> 
-              {showCalendar && (
-                <div className={styles.calendarPlaceholder}>
-                  {/* Aquí irá el calendario */}
-                  <p>Calendario se mostrará aquí</p>
-                </div>
-              )}
-            </div>
-
-            <h2>Resumen De Servicios</h2>
-
-            <div className={styles.summaryDetails}>
-              <div className={styles.summaryRow}>
-                <span>Subtotal:</span>
-                <span>${subtotal.toFixed(2)}</span>
-              </div>
-              <div className={styles.summaryRow}>
-                <span>IVA:</span>
-                <span>${iva.toFixed(2)}</span>
-              </div>
-              <div className={`${styles.summaryRow} ${styles.total}`}>
-                <span>Total:</span>
-                <span>${total.toFixed(2)}</span>
->>>>>>> 300293674121452163b021140472e18fea699c95
               </div>
             </div>
           ))}
@@ -481,7 +327,6 @@ const Bolsa = () => {
                 ? "Ocultar Formulario de Pago"
                 : "Mostrar Formulario de Pago"}
             </button>
-<<<<<<< HEAD
           ) : (
             <button
               className={styles.payButton}
@@ -505,15 +350,6 @@ const Bolsa = () => {
               />
             </Elements>
           )}
-=======
-           
-            {showForm && (
-              <Elements stripe={stripePromise}>
-                <CheckOutForm total={total} />
-              </Elements>
-            )}
-          </div>
->>>>>>> 300293674121452163b021140472e18fea699c95
         </div>
       </div>
     </div>
